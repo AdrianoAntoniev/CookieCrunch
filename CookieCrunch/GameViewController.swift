e8e794331e7876214f36883cd195fffe6ad3d2de
Adriano Rodrigues Vieira <adriano.antoniev@outlook.com>
2020-08-26 17:12:04 -0300

    Limit moves, only moves who can result in chaining is possible now

--- CookieCrunch/GameViewController.swift
+++ CookieCrunch/GameViewController.swift
@@ -115,9 +115,14 @@ class GameViewController: UIViewController {
   func handleSwipe(_ swap: Swap) {
     view.isUserInteractionEnabled = false
 
-    level.performSwap(swap)
-    scene.animate(swap) {
-      self.view.isUserInteractionEnabled = true
+    if level.isPossibleSwap(swap) {
+      level.performSwap(swap)
+      scene.animate(swap) {
+        self.view.isUserInteractionEnabled = true
+      }
+    } else {
+      view.isUserInteractionEnabled = true
     }
   }
+
 }
